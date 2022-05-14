178c8e17d7992d9a7b12d7ca54438284fe79ca17
Noor-26 <nk26982698@gmail.com>
2022-05-14 10:55:30 +0600

    banner created

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -3,12 +3,12 @@ import chairImg from '../../../images/images/chair.png';
 import bgImg from '../../../images/images/bg.png'
 const Banner = () => {
     return (
-        <div class={`hero min-h-screen bg-base-100 `}>
+        <div class="hero min-h-screen bg-base-100 " style={{backgroundImage: `url(${bgImg})`}} > 
   <div class="hero-content flex-col lg:flex-row-reverse">
     <img src={chairImg}class="max-w-sm rounded-lg shadow-2xl" />
     <div className="px-12">
       <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p class="py-6">Come visit our website and choose whatever package you want for your kids ,your loved ones or you yourself.</p>
       <button class="btn btn-primary font-bold text-white uppercase bg-gradient-to-r from-secondary to-primary hover:bg-gradient-to-l from-primary to-secondary">Get Started</button>
     </div>
   </div>
