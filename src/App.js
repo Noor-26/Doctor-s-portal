a4ebdf487d78ed5bbf82c13e9f2022807dcc525f
Noor-26 <nk26982698@gmail.com>
2022-05-14 10:33:28 +0600

    Navber created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home/Home';
+import Navber from './Components/Shared/Navber/Navber';
+import About from './Components/About/About';
+import Appointment from './Components/Appointment/Appointment';
+import Contact from './Components/Contact/Contact';
+import Login from './Components/Login/Login/Login';
+import Register from './Components/Login/Register/Register';
+import Review from './Components/Review/Review';
 
 function App() {
+
   return (
     <div className="App">
-   
+      <Navber/>
+   <Routes>
+     <Route path="/" element={<Home/>}/>
+     <Route path="/about" element={<About/>}/>
+     <Route path="/appointment" element={<Appointment/>}/>
+     <Route path="/login" element={<Login/>}/>
+     <Route path="/register" element={<Register/>}/>
+     <Route path="/Review" element={<Review/>}/>
+     <Route path="/contact" element={<Contact/>}/>
+   </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,31 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+      themes: [
+        {
+          mytheme: {
+          
+ "primary": "#0FCFEC",
+          
+ "secondary": "#19D3AE",
+          
+ "accent": "#19D3AE",
+          
+ "neutral": "#191D24",
+          
+ "base-100": "#fff",
+           
+ "info": "#3ABFF8",
+          
+ "success": "#36D399",
+          
+ "warning": "#FBBD23",
+          
+ "error": "#F87272",
+          },
+        },
+      ],
+    },
   plugins: [require("daisyui")],
 }
