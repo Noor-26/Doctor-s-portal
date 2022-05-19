ca1ff8347b993e72a1342ebd236cd47e2a9190cc
Noor-26 <nk26982698@gmail.com>
2022-05-19 12:27:22 +0600

    footer created

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import footerImg from '../../../images/images/footer.png'
 export default function Footer() {
   return (
-    <div>
-        <footer class="footer p-10 ">
+    <div style={{
+        background: `url(${footerImg})`,
+        backgroundSize: 'cover'
+    }} className="p-10">
+        <footer >
+  <div class="footer p-10 font-bold">
   <div>
     <span class="footer-title">Services</span> 
     <a class="link link-hover">Branding</a>
@@ -24,6 +28,10 @@ export default function Footer() {
     <a class="link link-hover">Privacy policy</a>
     <a class="link link-hover">Cookie policy</a>
   </div>
+  </div>
+  <div className='my-10 text-center'>
+                <p>Copyright © 2022 - All right reserved by Darkfam</p>
+            </div>
 </footer>
     </div>
   )
